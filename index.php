c228458ea801df650f095bcfe04249ed56a123a5
holms <anna61c9@edu.zealand.dk>
2023-05-12 12:34:47 +0200

    nav tilføjet.

--- index.php
+++ index.php
@@ -18,20 +18,7 @@
 <!-- Cover -->
 <img id="bg" src="images/waterCircle.webp" alt="">
 
-<!-- Brand -->
-<nav class="navbar navbar-light">
-    <div class="container-fluid justify-content-center d-flex">
-        <div class="col-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="row mt-2">
-
-                <a class="navbar-brand" href="index.html"><img src="images/logo.png" style=" width: 10% !important"
-                                                               class="img mx-auto" alt="brand"></a>
-            </div>
-        </div>
-    </div>
-</nav>
-
-<br><br><br><br><br>
+<?php include 'nav.php'; ?>
 
 <div class="underrubrik container-fluid justify-content-center">
     <div class="col">
--- nav.php
+++ nav.php
@@ -1,1 +1,21 @@
 <?php
+echo '
+    
+    
+    <!-- Brand -->
+<nav class="navbar navbar-light">
+    <div class="container-fluid justify-content-center d-flex">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="row mt-2">
+
+                <a class="navbar-brand" href="index.html"><img src="images/logo.png" style=" width: 10% !important"
+                                                               class="img mx-auto" alt="brand"></a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<br><br><br><br><br>
+    
+';
+?>
