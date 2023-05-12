e0e6ce4b62dff4594e1d7d0f69f6c5cc1d76da18
holms <anna61c9@edu.zealand.dk>
2023-05-12 14:16:11 +0200

    cards.

--- index.php
+++ index.php
@@ -35,20 +35,6 @@
 </div>
 <br><br><br><br><br>
 
-<!-- CTA gamle position.
-<div id="container-fluid d-flex">
-    <div class="row CTA">
-        <div class="col col-md-8 col-lg-12 mx-5">
-            <button class="learn-more">
-    <span class="circle" aria-hidden="true">
-      <span class="icon arrow"></span>
-    </span>
-                <a href="intercom.php"><span class="button-text">Intercom</span></a>
-            </button>
-        </div>
-    </div>
-</div>
--->
 
 <!-- Accordion -->
 <div class="container-fluid justify-content-center" style="width: 70%;">
@@ -140,6 +126,8 @@
     </div>
 </div>
 
+
+<!-- Social media -->
 <div class="container-fluid justify-content-end socialMedia">
     <div class="row">
         <div class="col m-2">
@@ -151,20 +139,83 @@
 </div>
 <br><br><br><br><br><br>
 
+
+<!-- Video -->
 <video width="100%" height="100%" controls>
     <source src="movie.mp4" type="video/mp4">
     <source src="movie.ogg" type="video/ogg">
     Your browser does not support the video tag.
 </video>
 
+
+<!-- Cards + links -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto:wght@300&display=swap"
+      rel="stylesheet">
+<link rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"/>
+
+<main>
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="card m-4 mx-auto">
+                <img src="https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80"
+                     alt="">
+                <div class="card-content">
+                    <h2>
+                        Header
+                    </h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste,
+                        voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card m-4 mx-auto">
+                <img src="https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80"
+                     alt="">
+                <div class="card-content">
+                    <h2>
+                        Header
+                    </h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste,
+                        voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card m-4 mx-auto">
+                <img src="https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80"
+                     alt="">
+                <div class="card-content">
+                    <h2>
+                        Header
+                    </h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt exercitationem iste,
+                        voluptatum, quia explicabo laboriosam rem adipisci voluptates cumque.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<!-- Links -->
 <br><br><br><br><br><br>
 <div class="container justify-content-center links">
     <div class="row text-center">
         <a href="url" style="color: #0c63e4;">link text</a>
-    </div><br>
+    </div>
+    <br>
     <div class="row text-center">
         <a href="url" style="color: #0c63e4;">link text</a>
-    </div><br>
+    </div>
+    <br>
     <div class="row text-center">
         <a href="url" style="color: #0c63e4;">link text</a>
     </div>
@@ -172,7 +223,7 @@
 
 <?php include 'bottom.php'; ?>
 
-
+</div>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
