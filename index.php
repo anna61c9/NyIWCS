29374686cb8568ffd4abdf4dbafa3112e0b39a27
holms <anna61c9@edu.zealand.dk>
2023-06-03 22:48:43 +0200

    responsive ændringer.

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 </head>
 <body>
 <!-- Cover -->
-<img class="bg position-absolute d-none d-mb-block d-lg-block" src="images/waterCircleNy.webp" alt="background">
-<img class="bg position-absolute d-block d-mb-none d-lg-none" src="images/waterCircleSmall.webp" alt="background">
+<img class="bg position-absolute d-none d-mb-none d-lg-block" src="images/waterCircleNy.webp" alt="background desktop">
+<img class="bg position-absolute d-block d-mb-block d-lg-none" src="images/waterCircleSmall.webp" alt="background mobil">
 <?php include 'nav.php'; ?>
 
 <div class="container-fluid col-12" style="height: 20rem;"></div>
@@ -35,30 +35,30 @@
 
     </div>
 </div>
-<div class="container-fluid col-12" style="height: 8rem;"></div>
+<div class="container-fluid col-12" style="height: 2rem;"></div>
 
-
-<!-- Accordion -->
-<div class="container-fluid justify-content-center" style="width: 60%;">
-
-    <!-- CTA -->
-    <div id="container-fluid d-flex">
-        <div class="row CTA">
-            <div class="col col-md-8 col-lg-12 mx-5">
-                <button class="learn-more">
+<!-- CTA -->
+<div id="container-fluid d-flex">
+    <div class="row CTA">
+        <div class="col col-md-8 col-lg-12 mx-5">
+            <button class="learn-more">
     <span class="circle" aria-hidden="true">
       <span class="icon arrow"></span>
     </span>
-                    <a href="intercom.php"><span class="button-text">Intercom</span></a>
-                </button>
-            </div>
+                <a href="intercom.php"><span class="button-text">Intercom</span></a>
+            </button>
         </div>
     </div>
+</div>
 
-    <div class="container-fluid col-12" style="height: 8rem;"></div>
+<!-- Accordion -->
+<div class="container-fluid justify-content-center">
+
+
+    <div class="container-fluid col-12" style="height: 10rem;"></div>
 
     <div class="row">
-        <div class="col-12 col-md-8 col-lg-12 mx-auto">
+        <div class="col-12 col-md-8 col-lg-8 mx-auto">
 
             <div class="accordion accordion-flush id=" accordionFlushExample
             ">
@@ -70,17 +70,17 @@
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target="#flush-collapseOne" aria-expanded="false"
                             aria-controls="flush-collapseOne">
-                        Overskrift #1
+                        Collaboration
                     </button>
                 </h2>
                 <div id="flush-collapseOne" class="accordion-collapse collapse show" aria-labelledby="flush-headingOne"
                      data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Fusce quis lectus quis sem lacinia nonummy.
-                        Proin mollis lorem non dolor. In hac habitasse platea dictumst. Nulla ultrices odio.
-                        Donec augue. Phasellus dui. Maecenas facilisis nisl vitae nibh.
-                        Proin vel seo est vitae eros pretium dignissim. Aliquam aliquam sodales orci.
-                        Suspendisse potenti. Nunc adipiscing euismod arcu. Quisque facilisis mattis lacus.
+                    <div class="accordion-body">To make sure as many as possible are able to use this revolutionary technology,
+                        Raymarine and IWCS have joined forces to open up exciting opportunities in the future.
+                        Together, they aim to facilitate widespread adoption,
+                        ensuring its transformative potential reaches a vast and diverse audience.
+                        This collaborative endeavor underscores the commitment of Raymarine
+                        and IWCS driving innovation and effective equipment to its u
                     </div>
                 </div>
             </div>
@@ -89,15 +89,18 @@
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target="#flush-collapseTwo" aria-expanded="false"
                             aria-controls="flush-collapseTwo">
-                        Overskrift #2
+                        Product
                     </button>
                 </h2>
                 <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
                      data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Fusce bibendum, velit in venenatis viverra, tellus ligula dignissim felis,
-                        quis euismod mauris tellus ut urna. Proin scelerisque. Nulla in mi. Integer ac leo.
-                        Nunc urna ligula, gravida a, pretium vitae, bibendum nec, ante. A
-                        liquam ullamcorper iaculis lectus. Sed vel dui.
+                    <div class="accordion-body">The iriSelect Black Box offers an effective solution to the challenge
+                        of creating a user-friendly, intuitive, and clutter-free instrument panel.
+                        This innovative technology allows for easy control of the iriSound
+                        Intelligent Intercom system through the existing Multifunction Functional Display (MFD).
+                        With all of the familiar functions of iriSelect available,
+                        users can now also effortlessly manage the recording function through iriStore,
+                        mobile phone, and hailer, all controlled via the MFD.
                     </div>
                 </div>
             </div>
@@ -106,36 +109,35 @@
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target="#flush-collapseThree" aria-expanded="false"
                             aria-controls="flush-collapseThree">
-                        Overskrift #3
+                        Screen layout
                     </button>
                 </h2>
                 <div id="flush-collapseThree" class="accordion-collapse collapse"
                      aria-labelledby="flush-headingThree"
                      data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Etiam lacinia risus vitae lacus. Aliquam elementum imperdiet turpis.
-                        In id metus. Mauris eu nisl. Nam pharetra nisi nec enim.
-                        Nulla aliquam, tellus sed laoreet blandit, eros urna vehicula lectus,
-                        et vulputate mauris arcu ut arcu. Praesent eros metus lirum larum,
-                        accumsan a, malesuada et, commodo vel, nulla. Aliquam sagittis auctor sapien. Morbi a nibh.
+                    <div class="accordion-body">The interface adapts to your Multi-Function Display,
+                        generating a layout based on your intercom configuration and connected devices.
+                        For fleets with uniform vessels and multiple users,
+                        IWCS can design a customized layout to optimize the user experience.
                     </div>
                 </div>
             </div>
+
+            <!-- Social media -->
+            <div class="container-fluid justify-content-end socialMedia">
+                <div class="row">
+                    <div class="col-12 m-2 col-md-8 col-lg-8">
+                        <a href="https://www.linkedin.com/company/iridiumwaterproof/?originalSubdomain=dk"><img
+                                    src="images/inHvid.png" class="img m-2" alt="linkedin"></a>
+                        <a href="https://www.youtube.com/@IWCSDenmark"><img src="images/youtube.png" class="img m-2" alt="Youtube"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid col-12" style="height: 14rem;"></div>
+
         </div>
     </div>
 </div>
-
-
-<!-- Social media -->
-<div class="container-fluid justify-content-end socialMedia">
-    <div class="row">
-        <div class="col m-2">
-            <a href="https://www.linkedin.com/company/iridiumwaterproof/?originalSubdomain=dk"><img
-                        src="images/inHvid.png" class="img m-2" alt="linkedin"></a>
-            <a href="https://www.youtube.com/@IWCSDenmark"><img src="images/youtube.png" class="img m-2" alt="Youtube"></a>
-        </div>
-    </div>
-</div>
-<div class="container-fluid col-12" style="height: 8rem;"></div>
 
 
 <!-- Video -->
@@ -150,10 +152,10 @@
 <!-- Cards -->
 <main>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row justify-content-center">
 
-            <div class="card m-3 mx-auto">
-                <img src="images/Cardmany.webp" alt="">
+            <div class="card m-3 col-md-12">
+                <img src="images/Cardmany.webp" alt="many to many">
                 <img src="images/card%20A.png"
                      alt="">
                 <div class="card-content">
@@ -166,8 +168,8 @@
                 </div>
             </div>
 
-            <div class="card m-3 mx-auto">
-                <img src="images/card2.webp" alt="">
+            <div class="card m-3 col-md-12">
+                <img src="images/card2.webp" alt="noise reduction">
                 <img src="images/card%20B.png"
                      alt="">
                 <div class="card-content">
@@ -180,8 +182,8 @@
                 </div>
             </div>
 
-            <div class="card m-3 mx-auto">
-                <img src="images/card4.webp" alt="">
+            <div class="card m-3 col-md-12">
+                <img src="images/card4.webp" alt="control panel">
                 <img src="images/card%20C.png"
                      alt="">
                 <div class="card-content">
