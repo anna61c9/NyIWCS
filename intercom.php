393c86f143e97ae9706fd3142a67f5194df6fe01
holms <anna61c9@edu.zealand.dk>
2023-06-03 23:36:34 +0200

    karrousel.

--- intercom.php
+++ intercom.php
@@ -33,14 +33,59 @@
 <div class="container-fluid position-relative">
     <div class="row p-2">
         <div class="col">
-            <a href="index.php"><img src="images/back.png" class="img" style="width: 3em" alt="back"></a>
+            <a href="index.php"><img src="images/back.png" class="img" style="width: 2em" alt="back"></a>
         </div>
     </div>
 </div>
 
 
+
+<div id="carouselExampleCaptions" class="carousel slide  d-block d-mb-none d-lg-none m-2" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="images/UdklipA.PNG" class="d-block w-100" alt="Interface">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Intercom controls</h5>
+                <p>Some representative placeholder content for the first slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="images/UdklipB.PNG" class="d-block w-100" alt="Interface">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Intercom custom</h5>
+                <p>Some representative placeholder content for the second slide.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="images/UdklipC.PNG" class="d-block w-100" alt="Interface">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Intercom settings</h5>
+                <p>Some representative placeholder content for the third slide.</p>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+
+
+
+
+
 <div class="container text">
-    <div class="row" style="height: 20rem;">
+    <div class="row" style="height: 2rem;">
     </div>
 </div>
 
