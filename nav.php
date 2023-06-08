06bdb4e65a0dbd1723d40458e730ab4ed80063f7
holms <anna61c9@edu.zealand.dk>
2023-06-09 00:06:16 +0200

    Logo centeret.

--- nav.php
+++ nav.php
@@ -3,9 +3,9 @@ echo '
     
     
     <!-- Brand -->
-<nav class="navbar navbar-light position-absolute" style="top: 0;">
-    <div class="container-fluid justify-content-center d-flex">
-        <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+<nav class="navbar navbar-light" style="top: 0;">
+    <div class="container-fluid justify-content-center">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-12 text-center">
             <div class="row mt-2">
                 <a class="navbar-brand " href="index.php"><img src="images/logo.png" style=" width: 6em; !important" class="img mx-auto" alt="brand"></a>
             </div>
