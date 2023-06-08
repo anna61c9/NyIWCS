3f3d7fd23d9f5b7d4a943ca3d4e2dcf05327fae8
holms <anna61c9@edu.zealand.dk>
2023-06-08 21:29:54 +0200

    New backDrop to interface

--- intercom.php
+++ intercom.php
@@ -12,7 +12,7 @@
     <!-- Titel som ses oppe i browserens tab mv. -->
     <title>Intercom</title>
 
-    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der ugiver siden og copyright information -->
+    <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
