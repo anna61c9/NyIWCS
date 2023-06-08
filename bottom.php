3d8bec71515e621161f9cea36d212c59527f9597
and20 <and2000mib@gmail.com>
2023-06-08 13:00:36 +0200

    opacity 0; bottom

--- bottom.php
+++ bottom.php
@@ -3,7 +3,7 @@
 <?php
 echo '
 
-<footer class="container-fluid d-flex position-relative m-0 p-0" style="background: #242741;">
+<footer class="container-fluid d-flex position-relative m-0 p-0" style="opacity: 0;">
     <div class="row col-12 p-5">
         <h5 class="text-center" style="color: white">(C) IWCS 2023, all rights reserved</h5>
         <h5 class="text-center" style="color: white">CVR 12 34 56 78</h5>
