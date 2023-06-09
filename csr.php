433cad8736cd0814059687ad777d978a24696cb2
Siw <spersson@hotmail.com>
2023-06-09 16:05:58 +0200

    bg rettet på csr

--- csr.php
+++ csr.php
@@ -17,18 +17,19 @@
 <body>
 
 <!-- Cover -->
+<img src="images/demoBackdrop.png" alt="background" class="csrbg position-absolute">
 
 <?php include 'nav.php'; ?>
 
 <div class="container-fluid position-relative">
     <div class="row p-3">
         <div class="col">
-            <a href="index.php"><img src="images/back.png" class="img" style="width: 2em" alt="back"></a>
+            <a href="index.php"><img src="images/back.png" class="img" style="width: 2em;" alt="back"></a>
         </div>
     </div>
 </div>
 
-<div class="container col-8">
+<div class="container position-relative col-8">
     <div class="row text-center p-5">
     <h1>Oops!...</h1>
     </div>
@@ -36,20 +37,21 @@
 
 <br>
 
-<div class="container" style="color:whitesmoke; width: 80vh; display: flex; justify-content: center; align-items: center;">
-    <img src="images/404.png" alt="404" class="img-fluid error">
+<div class="container position-relative" style="color:whitesmoke; width: 80vh; display: flex; justify-content: center; align-items: center;">
+    <img src="images/404.png" alt="404" class="img-fluid">
 </div>
 
 <br>
 
-<div class="container col-8 justify-content-center">
+<div class="container col-8 justify-content-center position-relative mt-4">
     <div class="row text-center p-5">
     <h3>Looks like you've stumbled upon uncharted audio territory.</h3>
         <h5>While our intercom system won't navigate you through this 404 page, it's busy saving lives with its incredible sound technology. Now, let's steer you back on track. Hit that 'arrow' button to embark on an exhilarating journey through our campaign website, where you'll discover how our life-saving intercom system revolutionizes communication. Together, we're amplifying safety, one conversation at a time!</h5>
     </div>
 </div>
 
-<div class="container-fluid col-12" style="height: 8rem;"></div>
+<div class="container-fluid col-12 position-relative" style="height: 20rem;"></div>
+
 <?php include 'bottom.php'; ?>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
--- handelsbetingelser.php
+++ handelsbetingelser.php
@@ -17,17 +17,17 @@ $handelsbetingelser = $db->sql("SELECT * FROM handelsbetingelser");
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
-    <link href="css/terms.css" rel="stylesheet" type="text/css">
-    
+    <link href="css/terms.scss" rel="stylesheet" type="text/css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
 <!-- Cover -->
+<img src="images/demoBackdrop.png" alt="background" class="position-absolute bg-image">
 
 <?php include 'nav.php'; ?>
-<div class="container-fluid col-12" style="height: 6rem;"></div>
+<div class="container-fluid col-12 position-relative" style="height: 6rem;"></div>
 <div class="seleclist container col-7 mt-2">
     <form action="/action_page.php">
         <label for="sel1" class="form-label">
@@ -52,82 +52,12 @@ $handelsbetingelser = $db->sql("SELECT * FROM handelsbetingelser");
 
 
 <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-<nav class="position-fixed navbar container sidenav d-none d-sm-none d-md-none d-lg-none">
 
-        <div class="container">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="#exclusivity-of-terms">EXCLUSIVITY OF TERMS</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#shipment-and-transportation">SHIPMENT AND TRANSPORTATION</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#acceptance-of-shipped-product">ACCEPTANCE OF SHIPPED PRODUCT</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#regional-regulatory-requirements">REGIONAL REGULATORY REQUIREMENTS</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#product-documentation">PRODUCT DOCUMENTATION</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#technical-compatibility">TECHNICAL COMPATIBILITY</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#re-export-restrictions">RE-EXPORT RESTRICTIONS</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#quotations">QUOTATIONS</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#force-majeure">FORCE MAJEURE</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#force-majeure">FORCE MAJEURE</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#frame-orders">FRAME ORDERS</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#payment-terms-&-invoicing">PAYMENT TERMS & INVOICING</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#payment-default">PAYMENT DEFAULT</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#taxes">TAXES</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#contract-development-and-customer-specific-projects">CONTRACT DEVELOPMENT AND CUSTOMER SPECIFIC PROJECTS</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#intellectual-property-rights">INTELLECTUAL PROPERTY RIGHTS</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#repairs">REPAIRS</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#disputes">DISPUTES</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#cancellation,-suspension,-or-modification-by-customer">CANCELLATION, SUSPENSION, OR MODIFICATION BY CUSTOMER</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#eu's-waste-electronic-and-electronic-equipment-(weee)-regulation-and-related-electronic-recycling-rules">EU'S WASTE ELECTRONIC AND ELECTRONIC EQUIPMENT (WEEE)</a>
-                </li>
-                <li class="nav-item">
-                    <a href="#warranty">WARRANTY</a>
-                </li>
-            </ul>
-        </div>
-</nav>
-
-<div class="container-fluid col-12" style="height: 2rem;"></div>
+<div class="container-fluid col-12 position-relative" style="height: 2rem;"></div>
 
 <?php foreach ($handelsbetingelser as $Allterms) { ?>
 
-    <main class="container-fluid"
+    <main class="container-fluid position-relative"
           id="<?php echo strtolower(str_replace(' ', '-', $Allterms->termsName)); ?>">
         <div class="row">
             <div class="container col-7 justify-content-center">
