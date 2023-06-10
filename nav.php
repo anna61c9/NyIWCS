f8e396ed19dac0089dac0ed860af0199834bcc36
Andreas Madsen <andr61j7@edu.zealand.dk>
2023-06-10 18:08:18 +0200

    logo center

--- nav.php
+++ nav.php
@@ -6,7 +6,7 @@ echo '
 <nav class="navbar navbar-light position-absolute d-none d-sm-none d-md-block d-lg-block" style="top: 0; width: 100%;">
     <div class="container-fluid">
         <div class="row mt-2">
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand" style="  transform: translateX(-50%); left: 50%; position: absolute;"  href="index.php">
                 <img src="images/logo.png" style="max-width: 9em; width: 100%; height: auto; display: block; margin: 0 auto;" class="img-fluid mx-auto d-block" alt="brand">
             </a>
         </div>
@@ -16,7 +16,7 @@ echo '
 <nav class="navbar navbar-light position-absolute d-block d-sm-block d-mb-none d-lg-none" style="top: 0; width: 100%;">
     <div class="container-fluid">
         <div class="row mt-2">
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand" style="  transform: translateX(-50%); left: 50%; position: absolute;" href="index.php" >
                 <img src="images/logo.png" style="max-width: 10vh; width: 100%; height: auto; display: block; margin: 0 auto;" class="img-fluid mx-auto d-block" alt="brand">
             </a>
         </div>
