fd047c5bde2a87d84fd5eced975dda36b507b6f1
holms <anna61c9@edu.zealand.dk>
2023-06-11 18:59:22 +0200

    iframe fejl

--- intercom.php
+++ intercom.php
@@ -102,7 +102,7 @@ background-size: cover; min-height: 100vh; background-position: center;">
 
 
 <div class="position-relative text-center d-none d-mb-block d-lg-block">
-    <iframe width="900" height="506,25" src="https://totallychaos.dk/Iwcs%20intercom/" title="DEMO" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="900" height="506,25" src="https://totallychaos.dk/Iwcs%20intercom/" title="DEMO" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
 <div class="container">
