1ceb56780bceccc8e142a487af2118a833868fa7
holms <anna61c9@edu.zealand.dk>
2023-06-17 10:39:44 +0200

    fjernet mellemrum

--- handelsbetingelser.php
+++ handelsbetingelser.php
@@ -28,8 +28,6 @@ background-size: cover; min-height: 100vh; background-position: center;">
 
 <?php include 'nav.php'; ?>
 
-<div class="container-fluid col-12" style="height: 0vh;"></div>
-
 <!-- Back -->
 <div class="container-fluid position-relative my-3">
     <div class="row p-3">
